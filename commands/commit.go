765275a8e0e94428d245451ad341b06216d466dd
Mazy <landonp95@gmail.com>
2022-09-21 13:14:47 +0000

    commit command, ihopefully workkking as intended

--- commands/commit.go
+++ commands/commit.go
@@ -1,5 +1,18 @@
 package commands
 
-func Commit() {
+import (
+	"fmt"
+	"os/exec"
+)
 
+func Commit(msg string) {
+	cmd := exec.Command("git", "commit", "-m", msg)
+	stdout, err := cmd.Output()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println(string(stdout))
 }
--- main.go
+++ main.go
@@ -8,12 +8,11 @@ import (
 	"os"
 )
 
+var commitCmd = flag.NewFlagSet("commit", flag.ExitOnError)
+var commitMsg = commitCmd.String("m", "", "Commit Message")
+
 func main() {
 	tools.OpSys()
-
-	commitCmd := flag.NewFlagSet("commit", flag.ExitOnError)
-	commitMsg := commitCmd.String("m", "", "Commit Message")
-
 	if len(os.Args) < 2 {
 		fmt.Println("Expected a command!")
 		os.Exit(1)
@@ -28,6 +27,6 @@ func main() {
 		commands.Add(os.Args[2])
 	case "commit":
 		commitCmd.Parse(os.Args[2:])
-		fmt.Println("  message: ", *commitMsg)
+		commands.Commit(*commitMsg)
 	}
 }
